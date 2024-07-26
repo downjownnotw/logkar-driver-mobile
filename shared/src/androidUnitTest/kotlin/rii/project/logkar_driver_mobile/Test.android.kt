3896e07f3c5cd62d05e2ecf9c866f6d6fc85eeba
raderido <rade.rido@logkar.com>
2024-07-26 19:52:21 +0700

    [development] init project

---
+++ shared/src/androidUnitTest/kotlin/rii/project/logkar_driver_mobile/Test.android.kt
@@ -0,0 +1,12 @@
+package rii.project.logkar_driver_mobile
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AndroidGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue("Check Android is mentioned", Greeting().greet().contains("Android"))
+    }
+}
---
+++ shared/src/commonTest/kotlin/rii/project/logkar_driver_mobile/Test.kt
@@ -0,0 +1,12 @@
+package rii.project.logkar_driver_mobile
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class CommonGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue(Greeting().greet().contains("Hello"), "Check 'Hello' is mentioned")
+    }
+}
